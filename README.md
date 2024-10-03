e3cec157bf4a88a17d3d3ebd3659ad284d101394
Fariborz Vahid <70701932+fariborzvahid@users.noreply.github.com>
2024-10-03 17:26:23 +0330

    init : project initiation commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is an TodoApp With Maui Hybrid 
+
+
---
+++ src/TodoApp.MauiClient/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApp.MauiClient
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
